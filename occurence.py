cf91fef424c81b1b85cf3b2a6339701a0c29f355
Yassine mazhare <137923652+Yassine-64@users.noreply.github.com>
2023-10-18 14:19:10 +0200

    Add files via upload

---
+++ occurence.py
@@ -0,0 +1,4 @@
+string = input("Your string here")
+char_to_count = input("entrer le caractere a compter :")
+occurrences = string.count(char_to_count)
+print("Number of occurrences of '", char_to_count, "':", occurrences)
